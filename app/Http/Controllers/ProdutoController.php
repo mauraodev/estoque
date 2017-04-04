dbf0d1b247df486cc8aa03da914970046b005921
Mauro Nunes <maurao.dev@gmail.com>
2017-04-03 23:20:50 -0300

    Adicionado controller e view para produtos

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace estoque\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class ProdutoController extends Controller
+{
+    public function lista()
+    {
+        $produtos = DB::select('select * from produtos');
+
+        return view('listagem')->withProdutos($produtos);
+    }
+}
---
+++ resources/views/listagem.blade.php
@@ -0,0 +1,19 @@
+<html>
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Controle de estoque</title>
+    <body>
+        <div class="container">
+        <h1>Listagem de produtos</h1>
+        <table class="table table-striped table-bordered table-hover">
+      <?php foreach ($produtos as $p): ?>
+      <tr>
+        <td><?=$p->nome?></td>
+        <td><?=$p->valor?></td>
+        <td><?=$p->descricao?></td>
+        <td><?=$p->quantidade?></td>
+      </tr>
+      <?php endforeach?>
+            </table>
+        </div>
+    </body>
+</html>
