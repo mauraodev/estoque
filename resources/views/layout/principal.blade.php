e4bc899efade915a0078883de998271bc1dd0992
Mauro Nunes <mauraoemau@hotmail.com>
2020-10-20 23:15:13 -0300

    Correção de url

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -11,12 +11,12 @@
 	    	<div class="container-fluid">
 
 	    		<div class="navbar-header">
-	      			<a class="navbar-brand" href="/produtos">Estoque Laravel</a>
+	      			<a class="navbar-brand" href="{{ url('/produtos') }}">Estoque Laravel</a>
 	    		</div>
 
 	      		<ul class="nav navbar-nav navbar-right">
-	        		<li><a href="/produtos">Listagem</a></li>
-	        		<li><a href="/produtos/novo">Novo</a></li>
+	        		<li><a href="{{ url('produtos') }}">Listagem</a></li>
+	        		<li><a href="{{ url('/produtos/novo') }}">Novo</a></li>
 	      		</ul>
 	    	</div>
 	  	</nav>
