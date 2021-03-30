7d24bde76f5769f5e2c6bd7ffcb6735bcec7cd29
Mauro Nunes <mauro.nunes@ikatec.com.br>
2021-03-30 16:34:07 -0300

    Move files to folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/custom.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/sass/custom.scss', 'public/css');
