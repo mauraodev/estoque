103d3ef35ab91bddd4fb2660e7457e3c68e6196d
Mauro Nunes <maurao.dev@gmail.com>
2021-07-06 15:35:57 -0300

    Sales view

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -5,6 +5,31 @@
 <div class="row">
     <div class="col-sm-12 col-md-12 col-lg-12">
         <h1>Vendas</h1>
+
+        <a href="{{ action("SalesController@create") }}" class="btn btn-primary">
+            Lançar venda!
+        </a>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Cliente</th>
+                    <th>Valor</th>
+                    <th>Data</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 
